69532d09d93b6c0a4b6c5b58d105796650cb95a2
MeesamBukhari <meesamturab@gmail.com>
2020-08-08 21:58:57 +0500

    Covered new things in JavaScript

---
+++ Class 12/app.js
@@ -0,0 +1,136 @@
+// var arr = ["cat", "dog", "tiger", "lion"];
+// console.log(arr[2]);
+
+
+
+// var arr1 = new Array("syed","meesam", "turab", "bukhari");
+// console.log(arr1);
+
+
+
+// arr[4] = "elephant";
+// console.log(arr);
+
+
+
+// var a;
+// for (a = 1; a <= 10; a++){
+//     document.write(a)
+// }
+
+
+
+// for (var b = 1; b <= 10; b++){
+//     document.write(b + "<br>")
+// }
+
+
+
+// for (var b = 1; b >= 1; b--){
+//     document.write(b)
+// }
+
+
+
+// var x;
+// for (x = 1; x <= 10; x = x+2){
+//     console.log(x);
+// }
+
+
+
+// for (var i = 0; i < 5; i++){
+//     alert("Hello World!")
+// }
+
+
+
+// document.write("2" + "x" + "01" + "=" + 2*01 + "<br>")
+// document.write("2" + "x" + "02" + "=" + 2*02 + "<br>")
+// document.write("2" + "x" + "03" + "=" + 2*03 + "<br>")
+// document.write("2" + "x" + "04" + "=" + 2*04 + "<br>")
+// document.write("2" + "x" + "05" + "=" + 2*05 + "<br>")
+
+
+
+// for (var i = 1; i <= 10; i++){
+//     document.write("2" + "x" + i + "=" + 2*i + "<br>")
+// }
+
+
+
+// var names = ["Syed", "Meesam", "Bukhari"];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+
+
+
+// var names = ["Syed", "Meesam", "Bukhari"];
+// for (var i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
+
+
+
+// var cities = ["Islamabad", "Lahore", "Karachi", "Faisalabad"];
+// for (var i = 0; i < cities.length; i++){
+//     if(cities[i] === "Islamabad"){
+//         alert(cities[i] + " is the Capital of Pakistan!");
+//         break
+//     }
+// }
+
+
+
+// for (var i = 0; i < 10; i++){
+//     if(i === 5){
+//         break
+//     }
+//     console.log("Hello World!")
+// }
+
+
+
+// for (var i = 0; i < 5; i++){
+//     for (var j = 0; j < 3; j++){
+//         document.write("Pakistan Zindabad" + "<br>")
+//     }
+// }
+
+
+
+// var firstNames = ["Syed", "Meesam", "Turab"]
+// var lastNames = ["Ali", "Bukhari"]
+// for (var i = 0; i < firstNames.length; i++){
+//     for (var x = 0; x < lastNames.length; x++)
+//         console.log(firstNames[i] + " " + lastNames[x])
+// }
+
+
+
+// for (var i = 0; i <= 10; i++){
+//     for (var j = 0; j < i; j++){
+//         document.write("")
+//     }
+//     document.write("<br>")
+// }
+
+
+
+// var word = prompt("Enter your word");
+// var check = "";
+// for (var i = word.length - 1; i >= 0; i--){
+//     check += word[i]
+// }
+// if (word === check){
+//     document.write(word + " is palindrome.")
+// }
+// else (document.write(word + " is not palindrome."))
+
+
+
+// var name = "Turab";
+// document.write(name[0])
+// document.write(name[1])
+// document.write(name[2])
