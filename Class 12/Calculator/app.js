3c1fa3950b520c065e47120642a6b97440793506
MeesamBukhari <meesamturab@gmail.com>
2020-08-09 23:10:58 +0500

    Calculator based Application created with JavaScript

---
+++ Class 12/Calculator/app.js
@@ -0,0 +1,32 @@
+// var val1 = +prompt("Enter Your First Value");
+// var sign = prompt("Enter Your Operator");
+// var val2 = +prompt("Enter Your Second Value");
+// console.log(val1);
+// console.log(val2);
+// console.log(sign);
+// console.log(val1 + " " + sign + " " + val2);
+// if(sign === "+"){
+//     alert((+val1) + (+val2))
+// }
+// else if(sign === "-"){
+//     alert(val1 - val2)
+// }
+// else if(sign === "*"){
+//     alert(val1 * val2)
+// }
+// else if(sign === "/"){
+//     alert(val1 / val2)
+// }
+// else if(sign === "%"){
+//     alert(val1 / val2 * 100 + " " + "%")
+// }
+
+
+
+// var equ = prompt("Enter Your Equation");
+// var num = equ.length;
+// for (var i = 0; i < num; i++){
+//     if(equ.slice(i, i+1) === "+" || equ.slice(i, i+1) === "-" || equ.slice(i, i+1) === "*" || equ.slice(i, i+1) === "/" ){
+//         alert("Sign Has Found") 
+//     }
+// }
