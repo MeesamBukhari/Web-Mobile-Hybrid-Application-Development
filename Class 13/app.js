4b881ccc0d653cb76ea9b0c9283485431abbed26
MeesamBukhari <meesamturab@gmail.com>
2020-08-11 23:09:20 +0500

    Covered new things in JavaScript

---
+++ Class 13/app.js
@@ -0,0 +1,16 @@
+// for (var a = 1; a <= 100; a = a + 10){
+//     for(var b = a; b < a + 10; b++){
+//         document.write(b + " ");
+//     }
+//     document.write("<br>")
+// }
+
+
+
+// var city = prompt("Enter Your City Name");
+// var arr = ["Karachi", "Islamabad", "Peshawar"];
+// for (var i = 0; i < arr.length; i++){
+//     if(arr[i] === city){
+//        console.log("City Has Found");
+//     }
+// }
