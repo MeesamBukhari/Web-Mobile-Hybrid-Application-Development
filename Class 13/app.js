75390bb5e36932c1662a6ab6366bab4affdab3ea
MeesamBukhari <meesamturab@gmail.com>
2020-08-15 19:03:29 +0500

    Covered new things in JavaScript

--- Class 13/app.js
+++ Class 13/app.js
@@ -1,5 +1,5 @@
-// for (var a = 1; a <= 100; a = a + 10){
-//     for(var b = a; b < a + 10; b++){
+// for (var a = 1; a <= 100; a = a + 10) {
+//     for (var b = a; b < a + 10; b++) {
 //         document.write(b + " ");
 //     }
 //     document.write("<br>")
@@ -8,9 +8,21 @@
 
 
 // var city = prompt("Enter Your City Name");
+// city = city.toLowerCase();
 // var arr = ["Karachi", "Islamabad", "Peshawar"];
-// for (var i = 0; i < arr.length; i++){
-//     if(arr[i] === city){
-//        console.log("City Has Found");
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === city) {
+//         alert("City Has Found");
 //     }
+//     else { alert("City Hasn't Found") }
 // }
+
+
+
+// var name = prompt("Enter your name");
+// // alert(name.toLowerCase());
+// name = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+// alert(name);
+
+
+
