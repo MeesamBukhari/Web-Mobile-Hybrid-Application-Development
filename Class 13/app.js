f304b08a26335d2a3a9f88f0285d9d3b6ee654f0
MeesamBukhari <meesamturab@gmail.com>
2020-08-15 19:59:21 +0500

    Covered new things in JavaScript

--- Class 13/app.js
+++ Class 13/app.js
@@ -8,7 +8,13 @@
 
 
 // var city = prompt("Enter Your City Name");
-// city = city.toLowerCase();
+// var firstChar = city.slice(0,1);
+// firstChar = firstChar.toUpperCase()
+// var otherChar = city.slice(1);
+// otherChar = otherChar.toLowerCase()
+// var city = firstChar + otherChar;
+// console.log(city)
+// // city = city.toLowerCase();
 // var arr = ["Karachi", "Islamabad", "Peshawar"];
 // for (var i = 0; i < arr.length; i++) {
 //     if (arr[i] === city) {
@@ -19,10 +25,28 @@
 
 
 
-// var name = prompt("Enter your name");
+// var name = prompt("Enter Your Name");
+// var firstChar = name.slice(0,1);
+// var otherChar = name.slice(1);
+// firstChar = firstChar.toUpperCase()
+// otherChar = otherChar.toLowerCase()
+// var name = firstChar + otherChar;
+// console.log(name)
 // // alert(name.toLowerCase());
+// alert(name);
+
+
+
+// var name = prompt("Enter Your Name");
 // name = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
 // alert(name);
 
 
 
+// var str = prompt("Enter Some Text");
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+//     if (str.slice(i, i + 2) === "  ") {
+//         alert("Double Spaces Are Found!");
+//     }
+// }
